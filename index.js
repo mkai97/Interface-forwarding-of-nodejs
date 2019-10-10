6983525dc2e8640f841dc612729e69047cec5f5a
Mkai <1345744510@qq.com>
2019-10-10 15:19:17 +0800

    添加打印本机ip地址

--- index.js
+++ index.js
@@ -22,7 +22,23 @@ app.use('/joke/content/list.php', jsonPlaceholderProxy);
 
 app.listen(3000);
 
-console.log('[DEMO] Server: listening on port 3000');
-console.log('[DEMO] Opening: http://localhost:3000/');
+// 获取本机ip地址利用导入os
+function getIPAddress(){
+    var interfaces = require('os').networkInterfaces();
+    for(var devName in interfaces){
+        var iface = interfaces[devName];
+        for(var i=0;i<iface.length;i++){
+            var alias = iface[i];
+            if(alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal){
+                return alias.address;
+            }
+        }
+    }
+  }
+  
+  const LOCAL_IP = getIPAddress()
+
+console.log('[DEMO] Server: listening on port 3000 or '+LOCAL_IP+':3000');
+console.log('[DEMO] Opening: http://localhost:3000/'+' or http://'+LOCAL_IP+':3000');
 
 // require('open')('http://localhost:3000/');

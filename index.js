e760e16e8ad703a21ac6647d046435d888866b34
‘“ <1345744510@qq.com>
2020-02-13 17:28:13 +0800

    增加接口

--- index.js
+++ index.js
@@ -61,11 +61,17 @@ var app = express();
 
 app.all("*", function (req, res, next) {
     //设置允许跨域的域名，*代表允许任意域名跨域
-    res.header("Access-Control-Allow-Origin", "*");
+    // res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "http://192.168.56.1:9527");
+    res.header('Access-Control-Allow-Credentials','true')
     //允许的header类型
     res.header("Access-Control-Allow-Headers", "content-type,x-token");
     //跨域允许的请求方式 
     res.header("Access-Control-Allow-Methods", "DELETE,PUT,POST,GET,OPTIONS");
+    res.header("Connection","keep-alive")
+    if(req){
+        console.log(req.query,'req')
+    }
     if (req.method.toLowerCase() == 'options')
     res.sendStatus(200)  //让options尝试请求快速结束
     else
@@ -86,6 +92,8 @@ app.get('/vue-element-admin/user/info', function (req, res) {
 app.use('/joke/content/list.php', jsonPlaceholderProxy);
 app.use('/joke/randJoke.php', jsonPlaceholderProxy);
 app.use('/toutiao/index', jsonPlaceholderProxy);
+
+
 app.use('/sys/authority/getAuthority', jsonPlaceholderProxy)
 app.use('/shop/cate/getCategoryList', jsonPlaceholderProxy)
 app.use('/sys/sysuser/login', jsonPlaceholderProxy)
@@ -94,7 +102,12 @@ app.use('/shop/cate/uploadAttachement', jsonPlaceholderProxy)
 app.use('/shop/good/getGoodList', jsonPlaceholderProxy)
 app.use('/shop/good/getGoodById',jsonPlaceholderProxy)
 app.use('/shop/cate/getAllCateListName',jsonPlaceholderProxy)
+app.use('/cy/business/content/getQuanShopGroupName',jsonPlaceholderProxy)
 app.use(`/shop/good/uploadAttachement`,jsonPlaceholderProxy)
+app.use(`/shop/good/deleteShopGood`,jsonPlaceholderProxy)
+app.use(`/shop/brand/getBrandList`,jsonPlaceholderProxy)
+app.use(`/shop/brand/uploadAttachement`,jsonPlaceholderProxy)
+app.use(`/shop/brand/deleteBrand`,jsonPlaceholderProxy)
 
 
 
